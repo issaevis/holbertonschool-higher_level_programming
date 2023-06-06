6a84c4d4de83036b06d4036cefc89943d52c9268
issaevis <evis.topollaj@gmail.com>
2023-06-06 15:21:49 +0200

    Added yet another fix to 7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
-    for num1 in matrix:
-        for i in range(len(num1)):
-            if i == len(num1) - 1:
-                print("{:d}".format(num1[i]), end="")
+    for num in matrix:
+        for i in range(len(num)):
+            if i == len(num) - 1:
+                print("{:d}".format(num[i]), end="")
             else:
-                print("{:d}".format(num1[i]), end=" ")
+                print("{:d}".format(num[i]), end=" ")
         print()
