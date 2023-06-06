0f6a1bb4e7039f2dbdb610accce5078f795be3e4
issaevis <evis.topollaj@gmail.com>
2023-06-06 15:19:08 +0200

    Fixed up 6 and 7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        for row in matrix:
-            print("\t".join(map(str, row)))
+    for num1 in matrix:
+        for i in range(len(num1)):
+            if i == len(num1) - 1:
+                print("{:d}".format(num1[i]), end="")
+            else:
+                print("{:d}".format(num1[i]), end=" ")
+        print()
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = (0,)
-    if len(tuple_b) == 0:
-        tuple_b = (0,)
-
-    result = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-    print(result)
+    tuple_a += (0, 0)[:2 - len(tuple_a)]
+    tuple_b += (0, 0)[:2 - len(tuple_b)]
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
