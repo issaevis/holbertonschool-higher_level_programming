f8586125e759333098538c0baec79307d2dab52e
issaevis <evis.topollaj@gmail.com>
2023-06-06 13:38:30 +0200

    Added exercise 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    new_list = my_list.copy()
+
+    new_list.sort(reverse=True)
+
+    return new_list[0]
