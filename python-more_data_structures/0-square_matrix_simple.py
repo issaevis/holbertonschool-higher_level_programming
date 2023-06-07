9512ad45a2d5da96038714d83fdd5219b503dd92
issaevis <evis.topollaj@gmail.com>
2023-06-07 11:53:36 +0200

    Fixed 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
+    if matrix is not None:
+        new_matrix = [row[:] for row in matrix]
 
-    for i in range(len(new_matrix)):
-        for j in range(len(new_matrix[i])):
-            new_matrix[i][j] = new_matrix[i][j] * new_matrix[i][j]
+        for i in range(len(new_matrix)):
+            for j in range(len(new_matrix[i])):
+                new_matrix[i][j] = new_matrix[i][j] * new_matrix[i][j]
 
-    return new_matrix
+        return new_matrix
