59ebf9a495dab41ce70973a1a7af4b7dd080cbf5
issaevis <evis.topollaj@gmail.com>
2023-06-07 18:10:10 +0200

    Added the last exercise

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return None
+
+    sum = 0
+    roman = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
+
+    i = 0
+    while i < len(roman_string):
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman:
+            sum += roman[roman_string[i:i+2]]
+            i += 2
+        else:
+            sum += roman[roman_string[i]]
+            i += 1
+
+    return sum
