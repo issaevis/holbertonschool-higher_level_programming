e5c0a1c5796b82ae5d72c92142556d8b0be60ade
issaevis <evis.topollaj@gmail.com>
2023-06-07 11:52:39 +0200

    Added 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+
+    for i in range(len(new_matrix)):
+        for j in range(len(new_matrix[i])):
+            new_matrix[i][j] = new_matrix[i][j] * new_matrix[i][j]
+
+    return new_matrix
