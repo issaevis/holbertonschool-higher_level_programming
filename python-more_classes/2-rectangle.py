cca70081cf73e5b3f65b1c762733a6a3c45c7f28
issaevis <evis.topollaj@gmail.com>
2023-06-13 16:13:25 +0200

    Added 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''module that creates an empty class called rectangle'''
+
+
+class Rectangle:
+    '''the rectangle class'''
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def perimeter(self):
+        '''returns the perimeter of the rectangle'''
+        return (2 * self.__height)+(2 * self.__width)
+
+    def area(self):
+        '''returns the area of the rectangle'''
+        return (self.__width * self.__height)
