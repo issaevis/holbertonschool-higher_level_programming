33d26939e1015c3b1611d256355af3d3411ec172
issaevis <evis.topollaj@gmail.com>
2023-06-16 14:10:23 +0200

    Added a final fix to 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,60 +1,69 @@
 #!/usr/bin/python3
-'''module to get access and update private attribute'''
+"""another square module """
 
 
 class Square:
-    '''class that gets the attributes for a square'''
+    '''init func'''
     def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if type(position) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__size = size
         self.__position = position
 
+    '''getter for size'''
     @property
     def size(self):
-        return (self.__size)
+        return self.__size
+
+    '''change size func'''
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (isinstance(value, tuple) and
-                len(value) == 2 and
-                all(v >= 0 for v in value)):
-            self.__position = value
-        else:
-            raise ValueError("position must be a tuple of 2 positive integers")
-
-    @size.setter
-    def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+    '''find area of square'''
     def area(self):
-        '''method to get the area of a square'''
-        return self.__size * self.__size
+        return self.__size ** 2
 
     def my_print(self):
-        '''method to print a square'''
-        if self.size == 0:
+        if self.__size == 0:
             print()
-        else:
-            if self.position[1] > 0:
-                pass
-
-            if self.position[0] > 0:
-                for i in range(self.size):
-                    for spc in range(self.position[0]):
-                        print("_", end="")
-                    for j in range(self.size):
-                        print("#", end="")
-                    print()
-            else:
-                for i in range(self.size):
-                    for j in range(self.size):
-                        print("#", end="")
-                    print()
+            return
+        for height in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for width in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                if j + 1 == self.__size:
+                    print('#')
+                else:
+                    print('#', end="")
