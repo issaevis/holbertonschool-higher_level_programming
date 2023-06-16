61752f370b795ca0c202a5026ccf43b508a47045
issaevis <evis.topollaj@gmail.com>
2023-06-16 14:41:36 +0200

    Added a fix to 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
-'''module that creates an empty class called rectangle'''
+"""Module for rectangle class"""
 
 
 class Rectangle:
-    '''the rectangle class'''
+    """Empty rectangle class"""
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__height = height
         self.__width = width
-
-    @property
-    def height(self):
-        return self.__height
 
     @property
     def width(self):
         return self.__width
 
-    @height.setter
-    def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
