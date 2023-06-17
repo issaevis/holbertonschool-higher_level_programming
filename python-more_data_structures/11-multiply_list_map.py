66b23e06183ad1c6eecd05fdc60721085242f87e
issaevis <evis.topollaj@gmail.com>
2023-06-17 15:40:35 +0200

    Added 0

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
+    if list == None:
+        return None
+    
+    if type(number) != int:
+        return None
+    
     return [el * number for el in my_list]
