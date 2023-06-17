61cfb64b07b5ce8f4cbf91e9b0c7552068f9ac5f
issaevis <evis.topollaj@gmail.com>
2023-06-17 16:17:30 +0200

    Fixed 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-
-elif len(sys.argv) == 1:
-    print("1 argument:\n{}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
-        if i > 0:
-            print("{}: {}".format(i, sys.argv[i]))
+    elif len(sys.argv) == 2:
+        print("1 argument:\n{}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print("{}: {}".format(i, sys.argv[i]))
