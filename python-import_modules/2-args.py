79a4a9d9b665cfacc79aaa0f6d8138d5b68faa12
issaevis <evis.topollaj@gmail.com>
2023-06-17 16:20:33 +0200

    Tried another fix to 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,8 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
-
     elif len(sys.argv) == 2:
-        print("1 argument:\n{}".format(sys.argv[1]))
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
