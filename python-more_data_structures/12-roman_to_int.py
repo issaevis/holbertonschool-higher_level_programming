c313b90d9250987aa39b581bcdcd891d6da2c129
issaevis <evis.topollaj@gmail.com>
2023-06-17 18:10:59 +0200

    Added a major fix to 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
-        return None
-
-    sum = 0
-    roman = [
-        ("M", 1000),
-        ("CM", 900),
-        ("D", 500),
-        ("CD", 400),
-        ("C", 100),
-        ("XC", 90),
-        ("L", 50),
-        ("XL", 40),
-        ("X", 10),
-        ("IX", 9),
-        ("V", 5),
-        ("IV", 4),
-        ("I", 1)
-    ]
-
+    rom_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+        'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40,
+        'XC': 90, 'CD': 400, 'CM': 900
+    }
+    num = 0
     i = 0
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman:
-            sum += roman[roman_string[i:i+2]]
-            i += 2
+        if roman_string[i:i+2] in rom_dict:
+            num += rom_dict[roman_string[i:i+2]]
+            i = i + 2
         else:
-            sum += roman[roman_string[i]]
-            i += 1
-
-    return sum
+            num += rom_dict[roman_string[i]]
+            i = i + 1
+    return num
