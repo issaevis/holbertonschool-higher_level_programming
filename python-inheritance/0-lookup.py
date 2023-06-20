b33f7041dc83e5399a25ec2ebedc2e97613893a4
issaevis <evis.topollaj@gmail.com>
2023-06-20 18:41:17 +0200

    Maybe fixed 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,4 +3,5 @@ def lookup(obj):
     '''Function that returns a list of available
     attributes and methods of an object'''
     lst = dir(obj)
+    lst = [item for item in lst if item != '__init_subclass__']
     return lst
