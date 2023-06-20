dd6496ce288bf593b156cc0f9157830ff6c45124
issaevis <evis.topollaj@gmail.com>
2023-06-20 18:43:42 +0200

    Maybe fixed 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+'''Function that returns a list of available
+attributes and methods of an object'''
+
+
 def lookup(obj):
-    '''Function that returns a list of available
-    attributes and methods of an object'''
+    '''Used dir to get the list'''
     return dir(obj)
