b68914954f9dd4e8b5e950ab6ecaac0c176f3745
issaevis <evis.topollaj@gmail.com>
2023-06-21 15:04:32 +0200

    Maybe fixed 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     '''reads a file and prints it'''
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         print(file.read())
