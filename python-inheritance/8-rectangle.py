62d2cfe452b2258d7979212a19d89c721c698c12
issaevis <evis.topollaj@gmail.com>
2023-06-21 12:14:50 +0200

    Definite fic for 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,21 +2,12 @@
 '''Module that creates a class the inherits from rectangle'''
 
 
-class BaseGeometry:
-    '''Class with a function'''
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''Class for creating rectangles'''
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        BaseGeometry.integer_validator(self, 'width', width)
+        BaseGeometry.integer_validator(self, 'height', height)
         self.__width = width
         self.__height = height
