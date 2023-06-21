c6ebfb2b8968cd24e487051417a8c610254668c7
issaevis <evis.topollaj@gmail.com>
2023-06-21 12:10:46 +0200

    Added 7 and 8. No test cases added yet. I plan on adding them as soon as I finish all the exercices. They are not the priority tbh

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module that creates a class'''
+
+
+class BaseGeometry:
+    '''Class with a function'''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
