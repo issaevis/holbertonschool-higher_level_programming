4fbec47143f2a10e848102d391a3c93238767f32
issaevis <evis.topollaj@gmail.com>
2023-06-21 11:53:14 +0200

    Added 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module that creates a class'''
+
+
+class BaseGeometry:
+    '''Class with a function'''
+    def area(self):
+        raise Exception("area() is not implemented")
