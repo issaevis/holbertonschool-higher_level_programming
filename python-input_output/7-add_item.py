ecafdb6be9bf182069dfa08411619a8ad63f5fc0
issaevis <evis.topollaj@gmail.com>
2023-06-22 13:30:29 +0200

    Fixed 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,18 +2,18 @@
 '''module to create a JSON file'''
 
 
-import json
+import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-if __name__ == "__main__":
 
-    my_list = []
-    i = 1
 
-    while i < len(sys.argv):
-        my_list.append(sys.argv[i])
-        i += 1
-
+if os.path.exists("add_item.json") is not True:
     with open("add_item.json", "w", encoding='utf-8') as file:
-        file.write(str(my_list))
+        my_list = []
+        save_to_json_file(my_list, "add_item.json")
+my_list = load_from_json_file('add_item.json')
+for i in range(len(sys.argv)):
+    if i != 0:
+        my_list.append(sys.argv[i])
+save_to_json_file(my_list, 'add_item.json')
