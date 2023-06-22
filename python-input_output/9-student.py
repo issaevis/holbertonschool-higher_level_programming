25301e7f5cd8a3931f3fc0413212100cb534a93f
issaevis <evis.topollaj@gmail.com>
2023-06-22 14:28:51 +0200

    Added 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''creates a class'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        d = dict()
+
+        for attr, value in self.__dict__.items():
+            d[attr] = value
+        return d
