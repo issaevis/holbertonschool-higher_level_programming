05f227ca2190a75f18e7c15d979242000497ff46
issaevis <evis.topollaj@gmail.com>
2023-06-26 13:51:29 +0200

    Adding a try to exercise 3. Not sure if it works. Locally I get some weird error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,16 +32,32 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError ("width must be an integer")
+        if width < 0:
+            raise ValueError ("width must be > 0")
         self.__width = width
 
     @height.setter
     def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError ("height must be an integer")
+        if height < 0:
+            raise ValueError ("height must be > 0")
         self.__height = height
 
     @x.setter
     def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError ("x must be >= 0")
         self.__x = x
 
     @y.setter
     def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError ("y must be >= 0")
         self.__y = y
