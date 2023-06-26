e9997d809c7d67cfa1812030f2c7ff33020f81c9
issaevis <evis.topollaj@gmail.com>
2023-06-26 13:57:25 +0200

    Added exercise 3. I think it works now, at least the output locally is gucci

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
     '''Class that defines a Rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -33,17 +33,17 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         if not isinstance(width, int):
-            raise TypeError ("width must be an integer")
+            raise TypeError("width must be an integer")
         if width < 0:
-            raise ValueError ("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @height.setter
     def height(self, height):
         if not isinstance(height, int):
-            raise TypeError ("height must be an integer")
+            raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError ("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @x.setter
@@ -51,7 +51,7 @@ class Rectangle(Base):
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
         if x < 0:
-            raise ValueError ("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @y.setter
@@ -59,5 +59,5 @@ class Rectangle(Base):
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
         if y < 0:
-            raise ValueError ("y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = y
