91ef68ae7348c00c568e9430783918dd49eadbe4
issaevis <evis.topollaj@gmail.com>
2023-06-26 18:54:24 +0200

    Added 11 and it maybe works?? I dunno will have to check the checker ahah

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,3 +14,12 @@ class Square(Rectangle):
         s = "[Square] ({}) {}/{}".format(self.id, self.x, self.y)
         s += " - {}".format(self.width)
         return s
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
