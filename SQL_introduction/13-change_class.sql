74fdeaf879c0f227a07de2a309194354bdf1d29a
issaevis <evis.topollaj@gmail.com>
2023-07-25 13:21:55 +0200

    Added a fix to 13

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- deletes all rows with a score lower than 5
-DELETE FROM second_table WHERE score < 5;
+-- deletes all rows with a score lower or equal than 5
+DELETE FROM second_table WHERE score <= 5;
