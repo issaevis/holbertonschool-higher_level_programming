524dab6954a888e196d285ed8cfdeaf0c1356e6a
issaevis <evis.topollaj@gmail.com>
2023-08-01 14:27:01 +0200

    Added 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,14 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=u, passwd=p, db=d,
                          charset='utf8')
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s",
-                (sys.argv[4], ))
+
+    query = " ".join([
+        "SELECT cities.id, cities.name, states.name",
+        "FROM cities JOIN states ON cities.state_id = states.id"
+        ])
+
+    cur.execute(query)
+
     states = cur.fetchall()
     for state in states:
         print(state)
