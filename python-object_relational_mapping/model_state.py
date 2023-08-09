4077bf6aad1f1bd322740a99d5c19bfa18d80c26
issaevis <evis.topollaj@gmail.com>
2023-08-09 15:12:20 +0200

    Added 6 again

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""module"""
-from sqlalchemy import create_engine, Column, String, Integer
+"""module for state class"""
+from sqlalchemy import create_engine, Column, String, Integer, Sequence
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
-    
+
+
 class State(Base):
+    '''state class'''
     __tablename__ = "states"
-    id = Column('id', Integer(), primary_key=True, autoincrement=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     name = Column('name', String(128), nullable=False)
-
-    def __init__(self, name):
-        self.name = name
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
