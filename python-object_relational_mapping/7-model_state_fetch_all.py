57f5328ceb9afd938596a20d010c713a16f2637e
issaevis <evis.topollaj@gmail.com>
2023-08-11 18:33:55 +0200

    Added a fix to 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-'''module to print all the values in a table'''
-from sys import argv
-from sqlalchemy import create_engine
-from model_state import State, Base
+"""model state class"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+import sys
 
 if __name__ == "__main__":
-    u = argv[1]
-    p = argv[2]
-    db = argv[3]
-
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(u, p, db), pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
     session = Session(engine)
-    for state in session.query(State).order_by(Stare.id).all():
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
